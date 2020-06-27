93b88ff1b767bab4ced9240f5349124bac3d1a56
Ryan Queenan <theq72@my.uri.edu>
2020-06-27 12:20:48 -0400

    Initial commit

---
+++ microservices/src/test/java/com/rq/ms/microservices/MicroservicesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rq.ms.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
